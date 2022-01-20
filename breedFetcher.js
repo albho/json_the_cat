1e17426d704c5be4724dffe047e96abb4ba9facb
Albert Ho <albertcyi.ho@gmail.com>
2022-01-20 13:51:08 -0800

    Completed tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,19 @@
 const request = require("request");
-const query = process.argv[2];
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${query}`,
-  function(error, response, body) {
-    const data = JSON.parse(body);
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    function (error, response, body) {
+      if (error !== null) return callback(error);
 
-    if (error) return console.log(`Error: ${error}`);
-    if (!data.length) return console.log(`Sorry, nothing found for ${query}`);
-    console.log(data[0].description);
-  }
-);
+      const data = JSON.parse(body);
+
+      if (!data.length) return callback(`${breedName} not found`, null);
+
+      const description = data[0].description;
+      callback(null, description);
+    }
+  );
+};
+
+module.exports = { fetchBreedDescription };
