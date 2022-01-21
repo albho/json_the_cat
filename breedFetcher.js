935374568ae30a86f9ef7a7f83408b11f9feb82e
Albert Ho <albertcyi.ho@gmail.com>
2022-01-20 17:13:56 -0800

    Minor syntax refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,4 +1,5 @@
 const request = require("request");
+<<<<<<< HEAD
 
 const fetchBreedDescription = function (breedName, callback) {
   request(
@@ -16,3 +17,17 @@ const fetchBreedDescription = function (breedName, callback) {
 };
 
 module.exports = { fetchBreedDescription };
+=======
+const query = process.argv[2];
+
+request(
+  `https://api.thecatapi.com/v1/breeds/search?q=${query}`,
+  function(error, response, body) {
+    const data = JSON.parse(body);
+
+    if (error) return console.log(`Error: ${error}`);
+    if (!data.length) return console.log(`Sorry, nothing found for ${query}`);
+    console.log(data[0].description);
+  }
+);
+>>>>>>> parent of 1e17426... Completed tests
